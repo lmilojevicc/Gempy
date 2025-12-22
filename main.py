3cc1c8a684e323b483d2d35ac13e6d06bf22c67e
Luka <104729643+lmilojevicc@users.noreply.github.com>
2025-12-22 02:31:21 +0100

    Refactor main

--- main.py
+++ main.py
@@ -9,19 +9,8 @@ from ai_config import AVAILABLE_FUNCS, MODEL_NAME, SYSTEM_PROMPT
 from functions.call_function import call_function
 
 
-def main():
-    load_dotenv()
-    api_key = os.environ.get("GEMINI_API_KEY")
-
-    parser = argparse.ArgumentParser(description="Chatbot")
-    parser.add_argument("user_prompt", type=str, help="User prompt")
-    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
-    args = parser.parse_args()
-
-    client = genai.Client(api_key=api_key)
-
-    user_prompt = args.user_prompt
-    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+def run_agent(client: genai.Client, user_prompt: str, verbose: bool) -> None:
+    messages = [types.Content(role="user", parts=[types.Part(text=user_prompt)])]
 
     for _ in range(20):
         response = client.models.generate_content(
@@ -49,17 +38,17 @@ def main():
         prompt_tokens = usage_metadata.prompt_token_count
         response_tokens = usage_metadata.candidates_token_count
 
-        if args.verbose:
+        if verbose:
             print(f"User prompt: {user_prompt}")
             print(f"Prompt tokens: {prompt_tokens}")
             print(f"Response tokens: {response_tokens}")
 
         function_responses = []
         for fc in response.function_calls or []:
-            result = call_function(fc, args.verbose)
+            result = call_function(fc, verbose)
             if not result:
                 raise Exception("fatal couldn't call a func")
-            elif args.verbose:
+            if verbose:
                 print(f"-> {result.parts[0].function_response.response}")
             function_responses.append(result.parts[0])
 
@@ -68,6 +57,22 @@ def main():
 
     else:
         print("Maximum iterations reached, no final response.")
+
+
+def main():
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+    args = parser.parse_args()
+
+    client = genai.Client(api_key=api_key)
+    user_prompt = args.user_prompt
+    verbose = args.verbose
+
+    run_agent(client, user_prompt, verbose)
 
 
 if __name__ == "__main__":
